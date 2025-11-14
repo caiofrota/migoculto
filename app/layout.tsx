a4d403e0f9829b9bf9fd15d8cf21b19d5573c77d
Caio Frota <caiofrota@gmail.com>
2025-11-14 14:38:33 -0300

    chore: initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import { Metadata } from "next";
+import "./globals.css";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    metadataBase: new URL("https://migoculto.com.br"),
+    title: {
+      default: "MigOculto",
+      template: `%s | MigOculto`,
+    },
+    description: "MigOculto sorteia amigos secretos de forma fácil e divertida!",
+    openGraph: {
+      type: "website",
+      url: `https://migoculto.com.br`,
+      title: "MigOculto",
+      description: "MigOculto sorteia amigos secretos de forma fácil e divertida!",
+      siteName: "MigOculto",
+      images: ["/images/logo-128.png"],
+    },
+    alternates: {
+      canonical: `https://www.migoculto.com.br/`,
+    },
+  };
+}
+
+type Props = {
+  children: React.ReactNode;
+};
+export default async function RootLayout({ children }: Props) {
+  return (
+    <html>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
