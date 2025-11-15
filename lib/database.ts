cee07f8f2cfd7f4af923c39887d2421479f0dce6
Caio Frota <caiofrota@gmail.com>
2025-11-15 17:57:01 -0300

    feat: database initial migration

---
+++ lib/database.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+export class PrismaFactory {
+  private static instance: PrismaClient;
+
+  public static getInstance(): PrismaClient {
+    if (!PrismaFactory.instance) {
+      PrismaFactory.instance = new PrismaClient();
+    }
+    return PrismaFactory.instance;
+  }
+}
