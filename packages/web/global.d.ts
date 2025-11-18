efb6cf21b37f95295392f0877f5a6f42ca01d2f4
Caio Frota <caiofrota@gmail.com>
2025-11-18 10:19:52 -0300

    chore: migrate to a monorepo

--- packages/web/global.d.ts
+++ packages/web/global.d.ts
@@ -1,6 +1,12 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     DATABASE_URL: string;
+    SMTP_HOST;
+    SMTP_PORT;
+    SMTP_USER;
+    SMTP_PASS;
+    SMTP_MAIL_TO;
+    NEXT_PUBLIC_BASE_URL: string;
     SESSION_SECRET: string;
     ACCESS_TOKEN_EXPIRES_IN: string;
     REFRESH_TOKEN_EXPIRES_IN: string;
