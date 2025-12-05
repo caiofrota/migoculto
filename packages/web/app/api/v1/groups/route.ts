2c03144b29c05cd345836f2393e47ae2b4928516
Caio Frota <caiofrota@gmail.com>
2025-12-05 16:13:16 -0300

    feat(group): implement update group functionality with notifications

--- packages/web/app/api/v1/groups/route.ts
+++ packages/web/app/api/v1/groups/route.ts
@@ -1,11 +1,15 @@
 import { getRequestUser } from "app/api/v1/session/authentication";
 import { withErrorHandling } from "errors/handler";
+import { addDoc, collection } from "firebase/firestore";
 import { prisma } from "lib/database";
+import { firestore } from "lib/firebase";
+import { sendGroupdChangeNotifications as sendGroupUpdateNotifications } from "lib/notification";
 import { NextRequest, NextResponse } from "next/server";
 import z from "zod";
 
 export const GET = await withErrorHandling(findAllUserGroups);
 export const POST = await withErrorHandling(createGroup);
+export const PUT = await withErrorHandling(updateGroup);
 
 async function findAllUserGroups(request: NextRequest) {
   const user = await getRequestUser(request);
@@ -127,7 +131,60 @@ async function createGroup(request: NextRequest) {
   return NextResponse.json(result, { status: 200 });
 }
 
+async function updateGroup(request: NextRequest) {
+  const user = await getRequestUser(request);
+  const params = await request.json();
+  const data = updateSchema.parse(params);
+
+  const group = await prisma.group.findFirst({
+    where: {
+      id: data.id,
+      ownerId: user.id,
+    },
+  });
+
+  if (!group) {
+    return NextResponse.json({ message: "Grupo não encontrado ou você não tem permissão para editar este grupo." }, { status: 404 });
+  }
+
+  const result = await prisma.group.update({
+    where: { id: data.id },
+    data: {
+      password: data.password,
+      name: data.name,
+      description: data.description,
+      eventDate: data.eventDate,
+      additionalInfo: data.additionalInfo,
+      location: data.location,
+    },
+    include: { members: { include: { user: { omit: { email: true, password: true }, include: { devices: true } } } } },
+  });
+
+  const refreshCollection = collection(firestore, "refresh");
+  addDoc(refreshCollection, {});
+
+  await sendGroupUpdateNotifications(
+    result,
+    result.members
+      .filter((m) => m.userId != user.id)
+      .map((m) => m.user.devices)
+      .flat(),
+  );
+
+  return NextResponse.json(result, { status: 200 });
+}
+
 const createSchema = z.object({
+  name: z.string().min(1).max(100),
+  password: z.string().min(4).max(20),
+  description: z.string().max(500).optional(),
+  eventDate: z.coerce.date(),
+  additionalInfo: z.string().max(500).optional(),
+  location: z.string().max(200).optional(),
+});
+
+const updateSchema = z.object({
+  id: z.number(),
   name: z.string().min(1).max(100),
   password: z.string().min(4).max(20),
   description: z.string().max(500).optional(),
